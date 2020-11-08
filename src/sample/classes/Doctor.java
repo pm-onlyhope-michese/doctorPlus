4b6f6c5b40d7333f7aa3a5872c47bd0e1d37d349
Michese <Michese48@mail.ru>
2020-11-08 14:17:29 +0300

    10

--- src/sample/classes/Doctor.java
+++ src/sample/classes/Doctor.java
@@ -7,6 +7,7 @@ public class Doctor {
 
     public Doctor(int id, String first_name, String last_name) {
         this.id = id;
+
         this.first_name = first_name;
         this.last_name = last_name;
     }
